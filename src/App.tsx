9258d644c306a61cf0a1c6e3182f2e620fd0d473
Julia Caroline <carolinejulia96@gmail.com>
2024-01-09 16:31:15 -0300

    correção de erros

--- src/App.tsx
+++ src/App.tsx
@@ -30,17 +30,17 @@ function App() {
   ]);
 
   return (
-    <div className='bg-gray-900 flex flex-col font-montserrat items-center justify-start mx-auto w-full'>
-      <header className="flex items-center justify-start w-full h-12 bg-white">
+    <div className='bg-gray-900 flex flex-col font-montserrat items-center justify-start mx-auto w-full md:w-screen md:overflow-hidden'>
+      <header className="flex items-center justify-center w-full h-12 bg-white md:h-10">
         <div>
-          <img src={logo_color} className="h-48 m-56" />
+          <img src={logo_color} alt='logo' className="h-48 m-56 md:h-40 md:m-20" />
         </div>
       </header>
       <section 
-        className='h-screen m-auto w-full bg-bg-image bg-cover'  
+        className='h-screen m-auto w-full bg-bg-image bg-cover md:bg-bg-mid sm:bg-bg-sm'  
       >
-        <div className='flex items-center h-screen ml-60'>
-          <div className='backdrop-blur-md bg-black/20 w-[480px] h-[480px] border-0 border-solid rounded-3xl flex flex-col justify-center items-center'>
+        <div className='flex items-center h-screen ml-60 md:ml-20 '>
+          <div className='backdrop-blur-md bg-black/20 w-[480px] h-[480px] border-0 border-solid rounded-3xl flex flex-col justify-center items-center sm:m-0'>
             <div>
               <h1 className='uppercase text-white font-extrabold text-3xl mx-10 text-center'>
                 proteção completa para o seu veículo
@@ -64,7 +64,7 @@ function App() {
                   className='m-2 p-4 rounded-md'
                 />
                 <SelectComp />
-                <button className='bg-red-700 py-4 rounded-lg text-white uppercase font-extrabold px-14 m-5 hover:transition-colors hover:duration-300 shadow-bs1 hover:shadow-red-700/80 shadow-red-700/50 hover:scale-[1.03]'>quero proteger meu veículo</button>
+                <button className='bg-red-700 py-4 rounded-lg text-white uppercase font-extrabold px-14 m-5 hover:animate-spin shadow-bs1 hover:shadow-red-700/80 shadow-red-700/50 hover:scale-[1.03]'>quero proteger meu veículo</button>
               </form>
             </div>
           </div>
@@ -75,31 +75,31 @@ function App() {
           <div className=''>
             <h1 className='uppercase text-3xl font-extrabold text-center text-blue'>benefícios que o associado <br /><span className='text-red-700'>alfa</span> tem</h1>
           </div>
-          <div className='flex items-center justify-center p-10'>
+          <div className='flex md:grid-2 md:w-screen items-center justify-center p-10'>
             <div className='flex flex-col items-center justify-center w-64 h-72 p-10 m-4 rounded-lg shadow-bs1 place-content-between text-center space-y-10 hover:transition-colors hover:duration-300 hover:shadow-lg hover:scale-[1.1]'>
-              <img src={crash} className='w-32' />
+              <img src={crash} alt='acidente' className='w-32' />
               <h5 className='uppercase font-extrabold text-blue'>proteção veicular e de vidros</h5>
             </div>
             <div className='flex flex-col items-center justify-center w-64 h-72 p-10 m-4 rounded-lg shadow-bs1 place-content-between text-center space-y-10 hover:transition-colors hover:duration-300 hover:shadow-lg hover:scale-[1.1]'>
-              <img src={phone} className='w-24'/>
+              <img src={phone} alt='telefone' className='w-24'/>
               <h5 className='uppercase font-extrabold text-blue'>assistência 24h</h5>
             </div>
             <div className='flex flex-col items-center justify-center w-64 h-72 p-10 m-4 rounded-lg shadow-bs1 place-content-between text-center space-y-10 hover:transition-colors hover:duration-300 hover:shadow-lg hover:scale-[1.1]'>
-              <img src={gps} className='w-24'/>
+              <img src={gps} alt='mapa' className='w-24'/>
               <h5 className='uppercase font-extrabold text-blue'>rastreamento</h5>
             </div>
             <div className='flex flex-col items-center justify-center w-64 h-72 p-10 m-4 rounded-lg shadow-bs1 place-content-between text-center space-y-10 hover:transition-colors hover:duration-300 hover:shadow-lg hover:scale-[1.1]'>
-              <img src={car} className='w-32'/>
+              <img src={car} alt='carro/moto' className='w-32'/>
               <h5 className='uppercase font-extrabold text-blue'>carro e moto reserva</h5>
             </div>
           </div>
           <div>
-            <button className='bg-red-700 py-4 rounded-lg text-white uppercase font-extrabold px-14 hover:transition-colors hover:duration-300 shadow-bs1 hover:shadow-red-700/80 shadow-red-700/50'>quero proteger meu veículo</button>
+            <button className='bg-red-700 py-4 rounded-lg text-white uppercase font-extrabold px-14 m-5 hover:animate-spin shadow-bs1 hover:shadow-red-700/80 shadow-red-700/50 hover:scale-[1.03]'>quero proteger meu veículo</button>
           </div>
         </div>
       </section>
-      <section className='flex items-center justify-center h-screen w-full bg-blue overflow-hidden'>
-        <div className='flex items-center justify-center'>
+      <section className='flex flex-col items-center justify-center h-screen w-full bg-blue overflow-hidden'>
+        <div className='flex items-center justify-center mb-10'>
             <div className='w-full h-full px-32 ml-20'>
               <div className=''>
                 <h1 className='text-white capitalize mb-10 font-extrabold text-[44px] text-center'>dúvidas frequentes</h1>
@@ -115,13 +115,17 @@ function App() {
             <div className='w-full h-full'>
               <img 
                 src={mobi}
+                alt='carro'
                 className='scale-[1.5]'  
               />
             </div>
         </div>
+        <div className='mt-10'>
+          <button className='bg-red-700 py-4 rounded-lg text-white uppercase font-extrabold px-14 hover:animate-spin shadow-bs1 hover:shadow-red-700/80 shadow-red-700/50 hover:scale-[1.03]'>quero proteger meu veículo</button>
+        </div>
       </section>
       <footer className='flex items-center justify-center w-full h-10 bg-[#353535]'>
-        <h2 className='text-white'>2024 Todos os direitos reservados©</h2>
+        <h2 className='text-white'>{(new Date().getFullYear())} - Todos os direitos reservados©</h2>
       </footer>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         'bg-image': "url('./assets/image1.jpg')",
+        'bg-mid': "url('./assets/imagemd.png')",
+        'bg-sm':  "url('./assets/imagesm.png')"
       },
       fontFamily: {
         'montserrat': ['"Montserrat"', 'sans-serif'],
